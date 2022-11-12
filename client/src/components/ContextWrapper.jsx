1680207fb2af8cf28142359cd9d11eb3a97e53f3
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2022-11-11 23:28:16 -0500

    added the dashboard view with layout and pre-made sections for navbar and userInfo. . In navbar.jsx and userinfo.jsx enter code where the comment tells you to do so.

--- client/src/components/ContextWrapper.jsx
+++ client/src/components/ContextWrapper.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import MyContext from "../contexts/MyContext";
 
 function ContextWrapper({ children }) {
-
+	const [loaded, setLoaded] = useState(true);
 	return (
 		<MyContext.Provider
 			value={{
-			
+				loaded,
+				setLoaded,
 			}}
 		>
 			{children}
