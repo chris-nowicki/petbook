55ff46b15d175600c1249c505aeae9fec50fb82c
Michael Start <103297119+Michael-Start@users.noreply.github.com>
2022-11-13 12:53:53 -0800

    App updated with routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,36 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard';
 import Chat from './components/Chat';
+import CreateCat from './components/CreateCat';
+import CreateDog from './components/CreateDog'
 import './App.css';
 import io from 'socket.io-client'
 import { useEffect, useState } from 'react';
 
 function App() {
 
-    // const socket = socketIO.connect('http://localhost:8001') Second try
-  const[socket] = useState(()=> io(':8001')); 
 
-  useEffect(()=>{
+  const [socket] = useState(() => io(':8001'));
+
+  useEffect(() => {
     console.log("Effect running")
-    socket.on("Welcome", data=> console.log(data));
-    return ()=> socket.disconnect(true);
+    socket.on("Welcome", data => console.log(data));
+    return () => socket.disconnect(true);
   }, [])
 
   return (
     <div className="App">
-      <h1>App</h1> 
+      <h1>App</h1>
       <BrowserRouter>
-      <div>
-        <Routes>
-          <Route path="/" element={ <Dashboard socket={socket} />}></Route>
-          <Route path="/chat" element={<Chat socket={socket} />}></Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+        <div>
+          <Routes>
+            <Route path="/" element={<Dashboard socket={socket} />}/>
+            <Route path="/chat" element={<Chat socket={socket} />}/>
+            <Route path='/createcat' element={<CreateCat />} />
+            <Route path='/createdog' element={<CreateDog />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
