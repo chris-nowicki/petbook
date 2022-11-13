3f422ed2ccfb67eaa65a5dbb79a1c1921150c647
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2022-11-13 17:16:51 -0500

    added socket.io-client as a dependency

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./views/Dashboard";
 import ContextWrapper from "./components/ContextWrapper";
