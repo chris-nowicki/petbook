7096f0ece4af0e866d79390d13c5e06f1815322e
Michael Start <103297119+Michael-Start@users.noreply.github.com>
2022-11-13 12:33:34 -0800

    Added controllers, routes, models cats/dogs

---
+++ server/controllers/dogs.controller.js
@@ -0,0 +1,48 @@
+const Dogs = require('../models/cats.model');
+const {request, response} = require('express');
+
+
+const DogsController = {
+    // Create description
+    create:(req,res)=>{
+        const{caption, picture} =req.body;
+        Dogs.create({
+            caption:caption,
+            picture:picture
+        })
+        .then(cat=>res.json(cat))
+        .catch(err=>res.status(400).json(err))
+    },
+    
+    //Read All
+    getAll:(req,res)=>{
+        Dogs.find({})
+        .then(cats=>{
+            res.json(cats)
+        })
+        .catch(err=>res.status(400).json(err))
+        
+    },
+    //Read One
+    getOne:(req,res)=>{
+        Dogs.findOne({_id:req.params.id})
+        .then(cat=>
+            res.json(cat))
+            .catch(err=>res.status(400).json(err))
+    },
+    //Update
+    update:(req,res)=>{
+        Dogs.findOneAndUpdate({_id:req.params.id}, req.body,{new:true, runValidators:true})
+        .then(updatedCaption =>res.json(updatedCaption))
+        .catch(err=>res.status(400).json(err))
+    },
+    //Delete
+    delete:(req,res)=>{
+        Dogs.deleteOne({_id:req.params.id})
+        .then(deleteConfirmation=> res.json(deleteConfirmation))
+        .catch(err=>res.json(err))
+    }
+
+}
+
+module.exports = DogsController
--- server/models/messages.model.js
+++ server/models/messages.model.js
@@ -7,11 +7,15 @@ const MessagesSchema = new mongoose.Schema({
     caption:{
         type:String,
         required:[true, 'Caption required'],
-        minlength:[8, 'Caption required']
+        minlength:[8, 'Caption must be 8 characters long']
+    },
+    user_id: {
+        type:String,
+        required:[true, 'Must be logged in to send message']
     }
     
 }, {timestamps: true})
 
-const modelName = mongoose.model(messages, MessagesSchema);
+const modelName = mongoose.model('messages', MessagesSchema);
 
 module.exports = modelName;
