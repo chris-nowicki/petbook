cabf10243deedc09fa414a3c5b96e4302ae50a3c
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2022-11-14 02:27:25 -0500

    default / route to login until landing page is fixed

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ function App() {
 			<ContextWrapper>
 				<Router>
 					<Routes>
-						{/* <Route path='/' element={<LandingPage />} default /> */}
+						<Route path='/' element={<Login />} default />
 						<Route path="/register" element={<Register />} />
 						<Route path="/login" element={<Login />} />
 
