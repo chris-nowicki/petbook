d27e267ddd8ed5c1eb21aac34942cfc442b577c5
IvanovaMargarita <ivanovamargarita711@gmail.com>
2022-11-13 19:49:10 -0500

    updated navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,12 +2,18 @@ import React from "react";
 
 function Navbar() {
 	return (
-		<div className="flex h-24 w-full flex-row border border-red-600 justify-between items-center p-4">
-			<div  className="   ">
-				petbook
+		<div className="flex h-24 w-full flex-row border border-red-600 justify-between items-center p-4 bg-orange-400">
+			<div  className="basis -1/4">
+				<h1 className="font-mono text-orange-50 text-2xl" >PetBook</h1>
 			</div>
-			<div  className="   ">
-				<button className="bg-blue-600 px-6 py-2 text-white hover:bg-purple-600"> test</button>
+			<div className="flex justify-end">
+				{/* <div  className=""> */}
+					<button className="bg-orange-50 px-6 py-2 text-orange-900 hover:bg-lime-100 rounded  border-orange-900"> create post</button>
+				{/* </div> */}
+				{/* <div  className="   "> */}
+					<button className="bg-orange-50 px-6 py-2 text-orange-900 hover:bg-lime-100 ml-3 border-orange-900 rounded"> logout</button>
+			{/* </div> */}
+
 			</div>
 		</div>
 	);
