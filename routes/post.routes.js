e8f87800fada4d40dc3d9784cf74f014e5296890
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2022-11-15 00:23:27 -0500

    removed rogue routes folder outside of the server/client directories.

--- routes/post.routes.js
+++
@@ -1,8 +0,0 @@
-const PostController = require("../controllers/post.controller");
-const { authenticate } = require("../config/jwt.config");
-
-module.exports = (app) => {
-	app.post("/api/posts", PostController.createPost);
-	app.get('/api/posts', PostController.getAll);
-	app.get('/api/posts/:id', PostController.getOne)
-};
