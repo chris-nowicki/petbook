cb8bfbeeef7650040865c7fe8185b965a6d6e53b
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2022-11-15 12:19:04 -0500

    manually pasting in changes

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -9,7 +9,60 @@ module.exports = {
 			})
 			.catch((err) => {
 				console.log(err);
-				res.json(err);
+				response.status(400).json(err);
 			});
+	},
+
+	getAllPosts: async (req, res) => {
+		Post.find({})
+			.then((posts) => {
+				res.json(posts);
+			})
+			.catch((err) => {
+				console.log(err);
+				response.status(400).json(err);
+			});
+	},
+
+	addComment: async (req, res) => {
+		const { id, user_id, fname, lname, comment } = req.body;
+		console.log(req.body);
+		Post.findOneAndUpdate(
+			{ _id: id },
+			{
+				$addToSet: {
+					comments: {
+						user_id: user_id,
+						userName: `${fname} ${lname}`,
+						comment: comment,
+					},
+				},
+			},
+			{
+				new: true,
+			}
+		)
+			.then((updatePost) => res.json(updatePost))
+			.catch((err) => res.status(400).json(err));
+	},
+
+	addLike: async (req, res) => {
+		const { id, user_id } = req.body;
+		console.log(req.body);
+		Post.findOneAndUpdate(
+			{ _id: id },
+			{
+				$addToSet: {
+					likes: {
+						user_id: user_id,
+					},
+				},
+			},
+			{
+				new: true,
+			}
+		)
+			.then((updatePost) => res.json(updatePost))
+			.catch((err) => res.status(400).json(err));
 	},
 };
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -2,5 +2,8 @@ const PostController = require("../controllers/post.controller");
 const { authenticate } = require("../config/jwt.config");
 
 module.exports = (app) => {
+	app.get("/api/posts", PostController.getAllPosts);
 	app.post("/api/posts", PostController.createPost);
+	app.put("/api/posts/add-comment", PostController.addComment);
+	app.put("/api/posts/add-like", PostController.addLike);
 };
