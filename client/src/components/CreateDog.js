dd7dd30c32f639577ad4ede8c0d7ecd7ac210fa9
IvanovaMargarita <ivanovamargarita711@gmail.com>
2022-11-14 20:26:14 -0500

    style for create dog component

--- client/src/components/CreateDog.js
+++ client/src/components/CreateDog.js
@@ -52,8 +52,30 @@ const CreateDog = () => {
         })
     }
     return(
-        <div className='App'>
-            <div className='Card'>
+
+        <div className='App   '>
+            {/* margarita's styling */}
+            <div className='flex h-screen w-5/6 flex-col items-center justify-center'>
+                <div className='flex flex-col rounded border border-black p-4 shadow-md shadow-black/25 items-center'>
+                    <img className='animaPicture  w-96' src ={picture} alt= 'A cute and snuggly pet!'/>
+                <button  className="w-32 bg-stone-300 px-4 py-2 my-3 rounded hover:bg-lime-100"onClick={getDog}>Get A Dog!</button>
+                <div className='getForm items-center justify-center'>
+                    <form className='' onSubmit={onSubmitHandler}>
+                        {/* <label><h4>Add a caption</h4></label> */}
+                        {errors.map((err,index)=><p key={index}>{err}</p>)}
+                        <input type = 'hidden' value ={picture}/>
+                        <textarea rows="3" className="block p-2.5 rounded-lg border border-gray-300" type = 'text' onChange={(e)=>setCaption(e.target.value)} placeholder="add a caption"/>
+                        <div>
+                            <p className='ml-4 text-sm'>*8 characters required</p>
+                        </div>
+                        <div>
+                            <button className='ml-4 bg-stone-300 px-4 py-2 my-3 rounded hover:bg-lime-100' type ='submit'>Save This Puppers!</button>
+                        </div>
+                    </form>
+                </div>
+                </div>
+            </div>
+            {/* <div className='Card '>
                 <img className='animaPicture' src ={picture} alt= 'A cute and snuggly pet!'/>
             </div>
             <button onClick={getDog}>Get A Dog!</button>
@@ -70,7 +92,7 @@ const CreateDog = () => {
                         <button type ='submit'>Save This Puppers!</button>
                     </div>
                 </form>
-            </div>
+            </div> */}
         </div>
     )
 
