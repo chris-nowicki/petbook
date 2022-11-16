dd0ec79cce0d446ce41bd6511f4212ee2db21d5d
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2022-11-16 13:02:03 -0500

    feat: like button now works with updated counts on the viewone page

--- client/src/components/ViewOne.jsx
+++ client/src/components/ViewOne.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
 import { useParams, useNavigate, Link, Navigate } from "react-router-dom";
 import MyContext from "../contexts/MyContext";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 
 const ViewOne = () => {
 	const [post, setPost] = useState([]);
@@ -10,59 +11,88 @@ const ViewOne = () => {
 	const [comments, setComments] = useState([]);
 	const [newComment, setNewComment] = useState("");
 	const [likes, setLikes] = useState([]);
-	const [pageContent,setPageContent] = useState("")
-	const [ author_id, setAuthor_id] = useState("")
+	const [pageContent, setPageContent] = useState("");
+	const [author_id, setAuthor_id] = useState("");
 	const { user, setShowFilter } = useContext(MyContext);
-	const navigate = useNavigate()
-	
-
+	const [clicked, setClicked] = useState(false)
+	const navigate = useNavigate();
 
 	useEffect(() => {
-		setShowFilter(false)
+		setShowFilter(false);
+
 		axios
 			.get(`http://localhost:8000/api/posts/${id}`)
 			.then((res) => {
 				setPost(res.data);
 				setComments(res.data.comments);
 				setLikes(res.data.likes);
-				setPageContent(res.data.content)
-				setAuthor_id(res.data.author_id)
+				setPageContent(res.data.content);
+				setAuthor_id(res.data.author_id);
 			})
 			.catch((err) => console.log(err));
 	}, []);
 
+	// check if the user is in the list of likes for the post
+	let liked = likes.some((like) => like.user_id === user._id);
+
+	// when user clicks like button this function runs
+	const handleLikes = () => {
+		axios
+			.put("http://localhost:8000/api/posts/add-like", {
+				id: post._id,
+				user_id: user._id,
+			})
+			.then((res) => {
+				// if response from the database is added, set clicked to true
+				// set clicked to true changes the color of the like button from gray to blue
+				if (res.data.message === "added") {
+					setClicked(true);
+					setLikes([...likes, {user_id: user._id}])
+				} else {
+					// if response from the database is not added then the like was deleted
+					// set clicked to false changes the color of the like button from blue to gray
+					setClicked(false);
+					let updatedLikes = [...likes]
+					updatedLikes = updatedLikes.filter(like => like.user_id !== user._id)
+					setLikes([...updatedLikes])
+				}
+				
+			});
+	};
+
 	//Add the visible className after :
-	const visible =user._id!= author_id ? "invisible" : ""
-		
-	
+	const visible = user._id != author_id ? "invisible" : "";
+
 	const handleUpdate = (e) => {
 		e.preventDefault();
-		axios.put(`http://localhost:8000/api/posts/update-content/${id}`, {
-			content:pageContent
-		})
-			.then(res => {
-				console.log(res)
-				setPageContent(res.data.content)
+		axios
+			.put(`http://localhost:8000/api/posts/update-content/${id}`, {
+				content: pageContent,
 			})
-			.catch(err => {
-				console.log(err)
+			.then((res) => {
+				console.log(res);
+				setPageContent(res.data.content);
+			})
+			.catch((err) => {
+				console.log(err);
 				const errorResponse = err.response.data.errors;
 				const errorArr = [];
 				for (const key of Object.keys(errorResponse)) {
-					errorArr.push(errorResponse[key].message)
+					errorArr.push(errorResponse[key].message);
 				}
-				setErrors(errorArr)
-			}, [])
-	}
+				setErrors(errorArr);
+			}, []);
+	};
 
 	const handleDelete = (_id) => {
-		axios.delete(`http://localhost:8000/api/posts/delete-post/${id}`)
+		axios
+			.delete(`http://localhost:8000/api/posts/delete-post/${id}`)
 			.then((res) => {
-				console.log(res)
-				navigate("/dashboard/feed")
+				console.log(res);
+				navigate("/dashboard/feed");
 			})
-			.catch(err => console.log(err))
-	}
+			.catch((err) => console.log(err));
+	};
 
 	// convert the date
 	//get date from when post was created at through the date props
@@ -99,16 +129,50 @@ const ViewOne = () => {
 					errorArr.push(errorResponse[key].message);
 				}
 			}, []);
-
-
-	}
+	};
 	return (
-		<div>
-			<h3>Posted by: {post.authorName}</h3>
-			<h4>Posted on: {month} {day}, {year}</h4>
-			<img src={post.postImage} alt ="Post image"></img>
-			{pageContent}
-			<p>Number of likes: {likes.length}</p>
+		<div className="flex h-screen w-full flex-col p-6">
+			<div className="mb-1 flex w-full flex-row items-center justify-between">
+				<h3>
+					<span className="ml-1 text-orange-400">
+						{post.authorName}{" "}
+					</span>
+					posted on{" "}
+					<span className="text-orange-400">
+						{month} {day}, {year}{" "}
+					</span>
+				</h3>
+				<div>
+					<button className="mr-2 hover:text-orange-400">edit</button>
+					|
+					<button
+						className="ml-2 mr-1 hover:text-orange-400"
+						onClick={handleDelete}
+					>
+						delete
+					</button>
+				</div>
+			</div>
+			<div className="flex w-full flex-col rounded bg-gray-200 p-4 shadow-md shadow-black/25">
+				<p className="ml-1 mb-4 text-lg">{pageContent}</p>
+
+				<img
+					src={post.postImage}
+					alt="Post image"
+					className="rounded"
+				/>
+				<div className="mt-2 flex w-full flex-row items-center justify-end">
+					<p className="mt-2 mr-1 font-bold text-orange-400">
+						{likes.length}
+					</p>{" "}
+					<button className="mx-1" onClick={() => handleLikes()}>
+						<ThumbUpIcon
+							color={liked || clicked ? "primary" : "action"}
+							fontSize="large"
+						/>
+					</button>
+				</div>
+			</div>
 			<h4>Comments</h4>
 			{comments.map((comment) => (
 				<>
@@ -118,6 +182,7 @@ const ViewOne = () => {
 					</div>
 				</>
 			))}
+
 			<form onSubmit={handleComment}>
 				{errors.map((err, index) => (
 					<p key={index}>{err}</p>
@@ -138,10 +203,15 @@ const ViewOne = () => {
 						<p key={index}>{err}</p>
 					))}
 					<label>Update Post Content: </label>
-					<input type="text-area" className="border border-black" onChange ={(e)=>setPageContent(e.target.value)} />
-					<button className="w-half border border-black bg-black py-3 text-xl text-white shadow-md shadow-black/25">Update</button>
+					<input
+						type="text-area"
+						className="border border-black"
+						onChange={(e) => setPageContent(e.target.value)}
+					/>
+					<button className="w-half border border-black bg-black py-3 text-xl text-white shadow-md shadow-black/25">
+						Update
+					</button>
 				</form>
-				<button onClick={handleDelete} className="w-half border border-black bg-black py-3 text-xl text-white shadow-md shadow-black/25">Delete Post</button>
 			</div>
 		</div>
 	);
